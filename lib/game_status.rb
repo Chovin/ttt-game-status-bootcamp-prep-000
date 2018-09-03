525728724849cfab7d19ddab8e2b31332e73cd5e
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-03 22:07:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,15 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+# horizontal
+WIN_COMBINATIONS = (0...9).each_slice(3).to_a
+# vertical
+WIN_COMBINATIONS.concat WIN_COMBINATIONS[0].zip *WIN_COMBINATIONS[1..-1]
+# diagonals
+WIN_COMBINATIONS << [0,4,8] << [6,4,2]
+
+def won?(board)
+  WIN_COMBINATIONS.find {|combo| 
+    ["XXX","OOO"].include? combo.map{|i| board[i]}.join
+  }
+end
